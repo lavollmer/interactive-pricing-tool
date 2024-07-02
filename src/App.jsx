5c0d7e12ff418a38420b1fe8ec45dbd0b273df7a
Laura <laura.e.vollmer@gmail.com>
2024-07-02 18:22:06 -0500

    adding extension in tailwindcss for styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
-        <h1 className="font-manrope">Simple, traffic-based pricing</h1>
+      <div className="font-15px font-manrope">
+        <h1>Simple, traffic-based pricing</h1>
         <h2>Sign-up for our 30-day trial.No credit card required.</h2>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     extend: {
       fontFamily: {
         'manrope': ['Manrope', 'sans-serif']
-      }
+      },
+      fontSize: {
+        '15px': '15px',
+      },
+      colors: {
+        // Primary colors
+        'soft-cyan': 'hsl(174, 77%, 80%)',
+        'strong-cyan': 'hsl(174, 86%, 45%)',
+        'light-grayish-red': 'hsl(14, 92%, 95%)',
+        'light-red': 'hsl(15, 100%, 70%)',
+        'pale-blue': 'hsl(226, 100%, 87%)',
+        // Neutral colors
+        'white': 'hsl(0, 0%, 100%)',
+        'very-pale-blue': 'hsl(230, 100%, 99%)',
+        'light-grayish-blue': {
+          DEFAULT: 'hsl(224, 65%, 95%)', // Default shade
+          'toggle': 'hsl(223, 50%, 87%)', // Differentiate if needed
+        },
+        'grayish-blue': 'hsl(225, 20%, 60%)',
+        'dark-desaturated-blue': 'hsl(227, 35%, 25%)',
+      },
     },
   },
   plugins: [],
