0aabd611b9fdbfc4bd66dbdefc51500eec2dd280
Laura <laura.e.vollmer@gmail.com>
2024-07-03 06:02:27 -0500

    added background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import bgPattern from "./assets/bg-pattern.svg";
 
 function App() {
   return (
     <>
-      <div className="font-15px font-manrope">
+      <div className="font-15px font-manrope" style={{backgroundImage: `url(${bgPattern})`}}>
         <h1>Simple, traffic-based pricing</h1>
         <h2>Sign-up for our 30-day trial.No credit card required.</h2>
       </div>
