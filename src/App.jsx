e96703a3f85b57c58bd950b7d51c99f104bd1904
Laura <laura.e.vollmer@gmail.com>
2024-07-04 23:55:59 -0500

    updating the README

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import bgPattern from "./assets/bg-pattern.svg";
 function App() {
   return (
     <>
-      <div className="font-15px font-manrope" style={{backgroundImage: `url(${bgPattern})`}}>
+      <div className="font-15px font-manrope w-full" style={{backgroundImage: `url(${bgPattern})`}}>
         <h1>Simple, traffic-based pricing</h1>
         <h2>Sign-up for our 30-day trial.No credit card required.</h2>
       </div>
