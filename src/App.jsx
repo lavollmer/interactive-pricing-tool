22170e014861aef2b0a802085cf4b0d061ca8e77
Laura <laura.e.vollmer@gmail.com>
2024-07-07 09:07:42 -0500

    debugging font and styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import bgPattern from "./assets/bg-pattern.svg";
 function App() {
   return (
     <>
-      <div className="font-15px font-manrope w-full" style={{backgroundImage: `url(${bgPattern})`}}>
-        <h1>Simple, traffic-based pricing</h1>
-        <h2>Sign-up for our 30-day trial.No credit card required.</h2>
+      <div className="w-full" style={{backgroundImage: `url(${bgPattern})`}}>
+        <h1 className="font-bold text-grayish-blue font-15px font-manrope">Simple, traffic-based pricing</h1>
+        <h2 className="font-bold text-grayish-blue font-15px font-manrope">Sign-up for our 30-day trial. No credit card required.</h2>
       </div>
     </>
   );
