79bac9d274daa76480b544b500a535a265e24d30
Laura <laura.e.vollmer@gmail.com>
2024-07-08 13:16:12 -0500

    styling page based on design replica

--- src/App.jsx
+++ src/App.jsx
@@ -15,89 +15,88 @@ function App() {
   return (
     <>
       {/* top part of the home page */}
-      <div className="flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
         <img
-          className="h-full w-full"
+          className="h-full w-full object-cover z-0"
           src={bgPattern}
           alt="Background pattern"
         />
-        <img
-          src={patternCircles}
-          alt="Background circles"
-          className="h-24 w-24 absolute justify-center items-center z-10 "
-        />
-        <div className="z-20 absolute flex flex-col items-center">
-          <h1 className="font-bold text-dark-desaturated-blue text-lg font-manrope">
-            Simple, traffic-based pricing
-          </h1>
-          <h2 className="text-grayish-blue text-sm font-manrope">
-            Sign-up for our 30-day trial. No credit card required.
-          </h2>
-          <div className="flex m-10">
-            <div className="bg-white shadow-lg rounded-md p-10">
-              <div className="flex flex-row space-x-12 items-center justify-center">
-                <h2 className="font-manrope font-bold text-xs text-grayish-blue">
-                  100K PAGE VIEWS
+        <div className="flex m-10 items-center justify-center">
+          <img
+            src={patternCircles}
+            alt="Background circles"
+            className="h-24 w-24 absolute justify-center items-center z-10 "
+          />
+          <div className="z-10 absolute flex flex-col items-center">
+            <h1 className="font-bold text-dark-desaturated-blue text-lg font-manrope">
+              Simple, traffic-based pricing
+            </h1>
+            <h2 className="text-grayish-blue text-sm font-manrope">
+              Sign-up for our 30-day trial. No credit card required.
+            </h2>
+          </div>
+        </div>
+        <div className="flex m-10">
+          <div className="bg-white shadow-lg rounded-md p-10">
+            <div className="flex flex-row space-x-12 items-center justify-center">
+              <h2 className="font-manrope font-bold text-xs text-grayish-blue">
+                100K PAGE VIEWS
+              </h2>
+              <div className="flex flex-row">
+                <h2 className="font-manrope font-bold text-2xl text-dark-desaturated-blue">
+                  $
                 </h2>
-                <div className="flex flex-row">
-                  <h2 className="font-manrope font-bold text-2xl text-dark-desaturated-blue">
-                    $
-                  </h2>
-                  <h2 className="font-manrope font-bold text-2xl text-dark-desaturated-blue">
-                    {value}
-                  </h2>
-                  <h2 className="font-manrope font-bold text-2xl text-dark-desaturated-blue">
-                    .00
-                  </h2>
-                  <h2 className="flex items-center justify-center font-manrope font-bold text-xs text-grayish-blue">
-                    /month
-                  </h2>
-                </div>
+                <h2 className="font-manrope font-bold text-2xl text-dark-desaturated-blue">
+                  {value}
+                </h2>
+                <h2 className="font-manrope font-bold text-2xl text-dark-desaturated-blue">
+                  .00
+                </h2>
+                <h2 className="flex items-center justify-center font-manrope font-bold text-xs text-grayish-blue">
+                  /month
+                </h2>
               </div>
-              <div className="flex m-6">
-                <input
-                  id="range"
-                  type="range"
-                  min="1"
-                  max="100"
-                  value={value}
-                  onChange={handleSliderChange}
-                  className="w-full h-2 bg-soft-cyan rounded-lg appearance-none cursor-pointer accent-bg-strong-cyan"
-                ></input>
+            </div>
+            <div className="flex m-6">
+              <input
+                id="range"
+                type="range"
+                min="1"
+                max="100"
+                value={value}
+                onChange={handleSliderChange}
+                className="w-full h-2 bg-soft-cyan rounded-lg appearance-none cursor-pointer accent-bg-strong-cyan"
+              ></input>
+            </div>
+            <div className="flex align-items items-center justify-center space-x-2 text-grayish-blue text-xs font-manrope">
+              <div className="flex flex-row items-center justify-center">
+                <h2>Monthly Billing</h2>
+                {/* Label element provides clikable area for checkbox and associate with label input, input has type of checkbox, first div is the background of the checkbox and the second div is the checkbox indicator */}
+                <label className="inline-flex items-center cursor-pointer rounded-full">
+                  {/* peer class is added to establish a reference for sibling elements, opacity-0 and absolute are to visually hide the checkbox */}
+                  <input type="checkbox" className="peer opacity-0 absolute" />
+                  <div className="relative w-11 h-6 bg-light-grayish-blue-toggle rounded-full"></div>
+                  <div className="absolute left-0 w-5 h-5 bg-white rounded-full shadow-md transform peer-checked:translate-x-full transition-transform"></div>
+                </label>
               </div>
-              <div className="flex align-items items-center justify-center space-x-2 text-grayish-blue text-xs font-manrope">
-                <div className="flex flex-row items-center justify-center">
-                  <h2>Monthly Billing</h2>
-                  {/* Label element provides clikable area for checkbox and associate with label input, input has type of checkbox, first div is the background of the checkbox and the second div is the checkbox indicator */}
-                  <label className="inline-flex items-center cursor-pointer rounded-full">
-                    {/* peer class is added to establish a reference for sibling elements, opacity-0 and absolute are to visually hide the checkbox */}
-                    <input
-                      type="checkbox"
-                      className="peer opacity-0 absolute"
-                    />
-                    <div className="relative w-11 h-6 bg-light-grayish-blue-toggle rounded-full"></div>
-                    <div className="absolute left-0 w-5 h-5 bg-white rounded-full shadow-md transform peer-checked:translate-x-full transition-transform"></div>
-                  </label>
-                </div>
-                <div className="flex flex-row items-center justify-center">
+              <div className="flex flex-row items-center justify-center">
                 <h2>Yearly Billing</h2>
                 <div className="bg-light-grayish-red rounded-full p-1 outline-style-solid">
                   <h2 className="text-light-red">25% discount</h2>
                 </div>
-                </div>
               </div>
-              <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
-              <div className="flex flex-row space-x-10 m-10">
-                <ul className="checkmark-list text-sm">
-                  <li>Unlimited websites</li>
-                  <li>100% data ownership</li>
-                  <li>Email reports</li>
-                </ul>
-                <div className="flex">
-                  <button className="pl-4 pr-4 mt-1 mb-1 w-full bg-dark-desaturated-blue text-white text-sm rounded-full">
-                    Start my trial
-                  </button>
-                </div>
+            </div>
+            <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+            <div className="flex flex-row space-x-10 m-10">
+              <ul className="checkmark-list text-sm">
+                <li>Unlimited websites</li>
+                <li>100% data ownership</li>
+                <li>Email reports</li>
+              </ul>
+              <div className="flex">
+                <button className="pl-4 pr-4 mt-1 mb-1 w-full bg-dark-desaturated-blue text-white text-sm rounded-full">
+                  Start my trial
+                </button>
               </div>
             </div>
           </div>
