01f6399c470dacb43e3f21c6927540b57827449d
Laura <laura.e.vollmer@gmail.com>
2024-07-08 11:51:51 -0500

    working on the copy content of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+//importing the background image from design folder
 import bgPattern from "./assets/bg-pattern.svg";
+//importing the circles image from design folder
 import patternCircles from "./assets/pattern-circles.svg";
 
 function App() {
   return (
     <>
+      {/* top part of the home page */}
       <div className="flex items-center justify-center">
         <img
           className="h-full w-full"
@@ -14,9 +17,8 @@ function App() {
         <img
           src={patternCircles}
           alt="Background circles"
-          className="h-20 w-20 absolute justify-center items-center z-10 "
+          className="h-24 w-24 absolute justify-center items-center z-10 "
         />
-
         <div className="z-20 absolute flex flex-col items-center">
           <h1 className="font-bold text-dark-desaturated-blue text-lg font-manrope">
             Simple, traffic-based pricing
@@ -24,6 +26,26 @@ function App() {
           <h2 className="text-grayish-blue text-sm font-manrope">
             Sign-up for our 30-day trial. No credit card required.
           </h2>
+          <div className="bg-white shadow-lg rounded-md">
+            <div>
+              <h1>100K Page Views</h1>
+              <h1>$16.00/month</h1>
+            </div>
+            <div>
+              <p>Slider</p>
+            </div>
+            <div>
+              <p>Monthly Billing</p>
+              <p>Yearly Billing</p>
+              <p>?% discount</p>
+            </div>
+            <div>
+              <p>Unlimited websites</p>
+              <p>1--% data ownership</p>
+              <p>Email reports</p>
+              <p>Start my trial</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
